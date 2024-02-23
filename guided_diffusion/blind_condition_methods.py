Scripts to run pnp-admm + fixes in the metric computation
Metrics were computed on grayscale images. This is no longer the case.

guided_diffusion/blind_condition_methods.py
@@ -13,6 +13,7 @@ from scipy.signal import convolve
 import numpy as np
 
 import torch.nn.functional as F
+from skimage.restoration import denoise_tv_chambolle
 
 __CONDITIONING_METHOD__ = {}
 
@@ -128,7 +129,7 @@ class Adam(BlindConditioningMethod):
         return x_0_hat, norm
     
 @register_conditioning_method(name='richardson-lucy')
-class MLEM(BlindConditioningMethod):
+class RL(BlindConditioningMethod):
     def __init__(self, operator, noiser, **kwargs):
         super().__init__(operator, noiser)
         assert kwargs.get('scale') is not None
@@ -164,6 +165,51 @@ class MLEM(BlindConditioningMethod):
         # plt.imshow(x_0_hat['img'].squeeze().detach().cpu().numpy().swapaxes(0, 1).swapaxes(1, 2))
         # plt.show()
         return x_0_hat, 0
+    
+    def conditioning(self, x_0_hat, measurement, steps, **kwargs):
+        
+        x_0_hat, norm = self.mlem(observation=measurement, x_0_hat=x_0_hat, steps=steps, clip=True, filter_epsilon=1e-6, device='cuda', **kwargs)
+
+        return x_0_hat, norm
+
+@register_conditioning_method(name='richardson-lucy-tv')
+class RLTV(BlindConditioningMethod):
+    def __init__(self, operator, noiser, **kwargs):
+        super().__init__(operator, noiser)
+        assert kwargs.get('scale') is not None
+        self.scale = kwargs.get('scale')
+        np.random.seed(123)
+    
+    def mlem(self, observation, x_0_hat, steps, clip, filter_epsilon, device, **kwargs):
+        with torch.no_grad():
+            img = x_0_hat['img']
+            kernel = x_0_hat['kernel'].repeat(1,3,1,1)
+            # kernel = x_0_hat['kernel']
+            
+            image = observation.to(torch.float32).clone().to(device)
+            psf = kernel.to(torch.float32).clone().to(device)
+            im_deconv = img.to(torch.float32).clone().to(device)
+            psf_mirror = torch.flip(psf, dims=[0, 1])
+
+            # Small regularization parameter used to avoid 0 divisions
+            eps = 1e-12
+            pad = (psf.size(2) // 2, psf.size(2) // 2, psf.size(3) // 2, psf.size(3) // 2)
+            for _ in range(steps):
+                conv = F.conv2d(F.pad(im_deconv, pad, mode='replicate'), psf) + eps
+                if filter_epsilon:
+                    relative_blur = torch.where(conv < filter_epsilon, torch.tensor(0.0, device=device), image / conv)
+                else:
+                    relative_blur = image / conv
+                im_deconv *= F.conv2d(F.pad(relative_blur, pad, mode='replicate'), psf_mirror)
+
+                im_deconv = torch.from_numpy(denoise_tv_chambolle(im_deconv.cpu().numpy(), weight=0.005, max_num_iter=50, channel_axis=1)).to(device)
+                
+            if clip:
+                im_deconv = torch.clamp(im_deconv, -1, 1)
+
+            x_0_hat['img'] = im_deconv.to(device)
+        
+            return x_0_hat, 0
     
     def conditioning(self, x_0_hat, measurement, steps, **kwargs):
         

guided_diffusion/gaussian_diffusion.py
@@ -446,74 +446,74 @@ class BlindDPS(DDPM):
         
         x_prev['img'] = self.q_sample(x_0_hat['img'], t=torch.tensor([self.num_timesteps-1] * batch_size, device=device))
         
-        with torch.no_grad():
+        # with torch.no_grad():
             
             # Deconvolution / Diffusion
             
-            for idx in pbar:
-                steps = 20
+            # for idx in pbar:
+            #     steps = 20
                         
-                # Normalize between 0 and 1 for Richardson-Lucy deconvolution
-                x_0_hat['img'] = (x_0_hat['img'] - x_0_hat['img'].min()) / (x_0_hat['img'].max() - x_0_hat['img'].min())
+            #     # Normalize between 0 and 1 for Richardson-Lucy deconvolution
+            #     x_0_hat['img'] = (x_0_hat['img'] - x_0_hat['img'].min()) / (x_0_hat['img'].max() - x_0_hat['img'].min())
                     
-                updated, norm = measurement_cond_fn(x_0_hat=x_0_hat,
-                                        measurement=measurement_norm,
-                                        steps=steps)
+            #     updated, norm = measurement_cond_fn(x_0_hat=x_0_hat,
+            #                             measurement=measurement_norm,
+            #                             steps=steps)
                 
-                x_0_hat['img'] = updated['img']
-                x_0_hat_RL = x_0_hat['img']
+            #     x_0_hat['img'] = updated['img']
+            #     x_0_hat_RL = x_0_hat['img']
 
-                time = torch.tensor([idx] * batch_size, device=device)
+            #     time = torch.tensor([idx] * batch_size, device=device)
                 
-                # Normalize between -1 and 1 for the diffusion model 
-                x_0_hat['img'] = (x_0_hat['img'] - x_0_hat['img'].min()) / (x_0_hat['img'].max() - x_0_hat['img'].min())
-                x_0_hat['img'] = x_0_hat['img'] * 2.0 - 1.0
+            #     # Normalize between -1 and 1 for the diffusion model 
+            #     x_0_hat['img'] = (x_0_hat['img'] - x_0_hat['img'].min()) / (x_0_hat['img'].max() - x_0_hat['img'].min())
+            #     x_0_hat['img'] = x_0_hat['img'] * 2.0 - 1.0
                 
                 
-                x_prev['img'] = self.q_sample(x_0_hat['img'], t=time)
-                mean, var, logvar = self.q_posterior_mean_variance(x_0_hat['img'], x_prev['img'], t=time)
+            #     x_prev['img'] = self.q_sample(x_0_hat['img'], t=time)
+            #     mean, var, logvar = self.q_posterior_mean_variance(x_0_hat['img'], x_prev['img'], t=time)
                 
-                x_prev['img'] = mean + var * torch.randn_like(mean)
-                output = dict() 
+            #     x_prev['img'] = mean + var * torch.randn_like(mean)
+            #     output = dict() 
                 
-                output['img'] = self.p_sample(x=x_prev['img'], t=time, model=model['img'])  
-                x_prev['img'] = output['img']['sample']
+            #     output['img'] = self.p_sample(x=x_prev['img'], t=time, model=model['img'])  
+            #     x_prev['img'] = output['img']['sample']
 
-                x_0_hat['img'] = output['img']['pred_xstart']
+            #     x_0_hat['img'] = output['img']['pred_xstart']
 
-                diff = (gt - x_0_hat['img'])**2
-                norm = diff.mean()
-                norm_array.append(norm.item())  # Append the norm value to the array
+            #     diff = (gt - x_0_hat['img'])**2
+            #     norm = diff.mean()
+            #     norm_array.append(norm.item())  # Append the norm value to the array
                 
-                if record:
-                    if idx % 1 == 0:
-                        save_dir = os.path.join(save_root, 'progress_x_0_hat/img/')
-                        if not os.path.isdir(save_dir): 
-                            os.makedirs(save_dir, exist_ok=True)
-                        file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
-                        plt.imsave(file_path, clear_color(x_0_hat['img']))
-                        plt.close()
+            #     if record:
+            #         if idx % 1 == 0:
+            #             save_dir = os.path.join(save_root, 'progress_x_0_hat/img/')
+            #             if not os.path.isdir(save_dir): 
+            #                 os.makedirs(save_dir, exist_ok=True)
+            #             file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
+            #             plt.imsave(file_path, clear_color(x_0_hat['img']))
+            #             plt.close()
                             
-                        save_dir = os.path.join(save_root, 'progress_x_t/img/')
-                        if not os.path.isdir(save_dir):
-                            os.makedirs(save_dir, exist_ok=True)
-                        file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
-                        plt.imsave(file_path, clear_color(x_prev['img']))
-                        plt.close()
+            #             save_dir = os.path.join(save_root, 'progress_x_t/img/')
+            #             if not os.path.isdir(save_dir):
+            #                 os.makedirs(save_dir, exist_ok=True)
+            #             file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
+            #             plt.imsave(file_path, clear_color(x_prev['img']))
+            #             plt.close()
                         
-                        save_dir = os.path.join(save_root, 'progress_diff/img/')
-                        if not os.path.isdir(save_dir):
-                            os.makedirs(save_dir, exist_ok=True)
-                        file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
-                        plt.imsave(file_path, clear_color(diff))
-                        plt.close()
+            #             save_dir = os.path.join(save_root, 'progress_diff/img/')
+            #             if not os.path.isdir(save_dir):
+            #                 os.makedirs(save_dir, exist_ok=True)
+            #             file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
+            #             plt.imsave(file_path, clear_color(diff))
+            #             plt.close()
                         
-                        save_dir = os.path.join(save_root, 'progress_RL/img/')
-                        if not os.path.isdir(save_dir): 
-                            os.makedirs(save_dir, exist_ok=True)
-                        file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
-                        plt.imsave(file_path, clear_color(x_0_hat_RL))
-                        plt.close()
+            #             save_dir = os.path.join(save_root, 'progress_RL/img/')
+            #             if not os.path.isdir(save_dir): 
+            #                 os.makedirs(save_dir, exist_ok=True)
+            #             file_path = os.path.join(save_dir, f"x_{str(idx).zfill(4)}.png")
+            #             plt.imsave(file_path, clear_color(x_0_hat_RL))
+            #             plt.close()
             
             # # Refinement with unconditional diffusion 
             
@@ -542,17 +542,22 @@ class BlindDPS(DDPM):
         # RL 
         
         # Normalize between 0 and 1 for Richardson-Lucy deconvolution
-        # steps = 500
+        # steps = 25
         
         # x_0_hat['img'] = (x_0_hat['img'] - x_0_hat['img'].min()) / (x_0_hat['img'].max() - x_0_hat['img'].min())
+        # plt.close()
             
         # updated, norm = measurement_cond_fn(x_0_hat=x_0_hat,
         #                         measurement=measurement_norm,
         #                         steps=steps)
         
+        plt.imsave(os.path.join(save_root, 'recon', 'intermediate.png'), clear_color(x_0_hat['img']))
         # x_0_hat['img'] = updated['img']
         
+        # plt.imshow(clear_color(x_0_hat['img']))
+        # plt.show()
         # x_prev['img'] = self.q_sample(x_0_hat['img'], t=torch.tensor([self.num_timesteps-1] * batch_size, device=device))
+        
         
         # Refinement with diffusion posterior conditioning
             

pnp_admm_dps_deblur.py
@@ -1,4 +1,4 @@
-from functools import partial
+
 import os
 import argparse
 import yaml
@@ -8,9 +8,6 @@ import torch
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
-from guided_diffusion.blind_condition_methods import get_conditioning_method
-from guided_diffusion.measurements import get_operator, get_noise
-
 # Here replaces the regular unet by our trained unet
 # from guided_diffusion.unet import create_model
 import guided_diffusion.diffusion_model_unet 
@@ -18,10 +15,20 @@ import guided_diffusion.unet
 
 from guided_diffusion.gaussian_diffusion import create_sampler
 from data.dataloader import get_dataset, get_dataloader
-from motionblur.motionblur import Kernel
-from util.img_utils import Blurkernel, clear_color
-from util.logger import get_logger
+from guided_diffusion.blind_condition_methods import get_conditioning_method
+from guided_diffusion.measurements import get_operator, get_noise
 
+from motionblur.motionblur import Kernel
+from util.img_utils import Blurkernel, clear_color, normalize_np
+from util.logger import get_logger
+from functools import partial
+
+import scico.numpy as snp
+from scico import functional, linop, loss, metric, plot, random
+from scico.optimize.admm import ADMM, LinearSubproblemSolver
+from scico.util import device_info
+
+import jax
 
 def load_yaml(file_path: str) -> dict:
     with open(file_path) as f:
@@ -39,7 +46,7 @@ def main():
     
     # Training
     parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--save_dir', type=str, default='./results/ffhq/ablation/rl-diffusion-dps-refinement/')
+    parser.add_argument('--save_dir', type=str, default='./results/ffhq/ablation/pnp-admm-dps-refinement/')
     
     # Regularization
     parser.add_argument('--reg_scale', type=float, default=0.1)
@@ -134,6 +141,19 @@ def main():
     torch.manual_seed(123)
     torch.backends.cudnn.deterministic = True  # if using CUDA
     
+    
+    if args.kernel == 'motion_blur':
+        kernel = Kernel(size=(args.kernel_size, args.kernel_size), intensity=args.intensity).kernelMatrix
+        kernel = torch.from_numpy(kernel).type(torch.float32)
+        kernel = kernel.to(device).view(1, 1, args.kernel_size, args.kernel_size)
+    elif args.kernel == 'gaussian_blur':
+        conv = Blurkernel('gaussian', kernel_size=args.kernel_size, std=args.intensity, device=device)
+        kernel = conv.get_kernel().type(torch.float32)
+        kernel = kernel.to(device).view(1, 1, args.kernel_size, args.kernel_size)
+                
+    psf = snp.array(kernel.squeeze().unsqueeze(2).cpu().numpy())  # convert to jax array
+    A = linop.Convolve(h=psf, input_shape=(256, 256, 3))
+    
     # Do Inference
     for i, ref_img in enumerate(loader):
         if(i==20):
@@ -141,21 +161,51 @@ def main():
             fname = str(i).zfill(5) + '.png'
             ref_img = ref_img.to(device)
 
-            if args.kernel == 'motion_blur':
-                kernel = Kernel(size=(args.kernel_size, args.kernel_size), intensity=args.intensity).kernelMatrix
-                kernel = torch.from_numpy(kernel).type(torch.float32)
-                kernel = kernel.to(device).view(1, 1, args.kernel_size, args.kernel_size)
-            elif args.kernel == 'gaussian_blur':
-                conv = Blurkernel('gaussian', kernel_size=args.kernel_size, std=args.intensity, device=device)
-                kernel = conv.get_kernel().type(torch.float32)
-                kernel = kernel.to(device).view(1, 1, args.kernel_size, args.kernel_size)
             
             # Forward measurement model (Ax + n)
             # y = operator.forward(ref_img, kernel)
             y = operator.forward(ref_img)
             y_n = noiser(y)
             
-            x_start = {'img': y_n,
+            x_gt = snp.array(ref_img.cpu().numpy().squeeze().swapaxes(0, 1).swapaxes(1, 2))  # convert to jax array
+
+        
+            sigma = 0.05  # noise level
+            
+            Ax = A(x_gt)  # blurred image
+            
+            noise, key = random.randn(Ax.shape)
+            y = Ax + sigma * noise
+            
+            f = loss.SquaredL2Loss(y=y, A=A)
+            g = functional.DnCNN("17M")
+            C = linop.Identity(x_gt.shape)
+
+            rho = 0.2  # ADMM penalty parameter
+            maxiter = 12  # number of ADMM iterations
+
+            solver = ADMM(
+                f=f,
+                g_list=[g],
+                C_list=[C],
+                rho_list=[rho],
+                x0=A.T @ y,
+                maxiter=maxiter,
+                subproblem_solver=LinearSubproblemSolver(cg_kwargs={"tol": 1e-3, "maxiter": 30}),
+                itstat_options={"display": True},
+            )
+
+            nc = 61 // 2
+            y = snp.clip(y[nc:-nc, nc:-nc], 0, 1)
+            
+            x = solver.solve()
+            x = snp.clip(x, 0, 1)
+    
+            x = np.array(x)
+            x = torch.from_numpy(x).to(device)
+            x = x.permute(2, 0, 1).unsqueeze(0)
+            
+            x_start = {'img': x,
                     'kernel': kernel}
 
             # !prior check: keys of model (line 74) must be the same as those of x_start to use diffusion prior.

util/compute_metric.py
@@ -28,229 +28,125 @@ loss_fn_vgg = lpips.LPIPS(net='vgg').to(device) # closer to "traditional" percep
 def stats(label_root, recon_root):
 
     psnr_normal_list = []
-    crc_list = []
     psnr_img_list = []
     ssim_img_list = []
     lpips_vgg_list = []
     lpips_alex_list = []
-    
-    crc_component_list = []
-    area_component_list = []
-    
+
     res_dict = {}
     
-    for idx in tqdm(range(11)):
+    for idx in tqdm(range(1000)):
         fname = str(idx).zfill(5)
 
-        label = Image.open(os.path.join(label_root, f'{fname}.png')).convert("L")
+        label = Image.open(os.path.join(label_root, f'{fname}.png')).convert('RGB')
         label = np.asarray(label) 
-        recon = Image.open(os.path.join(recon_root, f'{fname}.png')).convert("L")
+        recon = Image.open(os.path.join(recon_root, f'{fname}.png')).convert('RGB')
         recon = np.asarray(recon) 
-        
 
+        label_tensor = torch.tensor(label.swapaxes(1, 2).swapaxes(0, 1)).unsqueeze(0).to(device)
+        recon_tensor = torch.tensor(recon.swapaxes(1, 2).swapaxes(0, 1)).unsqueeze(0).to(device)
         
-        # # Apply a simple threshold to create a binary mask
-        # _, binary_mask = cv2.threshold(label, 65, 255, cv2.THRESH_BINARY)
-        
-        # # Find contours in the binary mask
-        # contours, _ = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-
-        # activity_label_img = 0
-        # activity_recon_img = 0
-        
-        # skip = False
-        
-        # # Iterate over each contour
-        # for contour in contours:
-        #     # Create a mask for each contour
-            
-        #     mask = np.zeros_like(label)
-            
-        #     cv2.drawContours(mask, [contour], 0, 255, thickness=cv2.FILLED)
-
-        #     area = np.count_nonzero(mask)
-            
-        #     if(area > 1000):
-        #         skip = True
-        #         break
-        #     # Extract the sub-region from both label and normal_recon using the mask
-            
-        #     label_component = cv2.bitwise_and(label, label, mask=mask)
-        #     normal_recon_component = cv2.bitwise_and(recon, recon, mask=mask)
-            
-        #     # PSNR
-        #     psnr_component = peak_signal_noise_ratio(label_component, normal_recon_component)
-        #     psnr_normal_list.append(psnr_component)
-            
-        #     # Activities
-            
-        #     activity_label_component = label_component.mean()
-        #     activity_recon_component = normal_recon_component.mean()
-            
-        #     activity_label_img += activity_label_component
-        #     activity_recon_img += activity_recon_component
-
-        #     crc_component = activity_recon_component / activity_label_component
-        #     crc_err_component = np.abs(1 - crc_component)
-            
-        #     crc_component_list.append(crc_err_component)
-        #     area_component_list.append(area)
-            
-        # # Concentration Recovery Coefficient 
-        # if skip:
-        #     continue
-        
-        # crc_img = activity_recon_img / activity_label_img
-        # crc_err = np.abs(1 - crc_img)
-
         # PSNR on the whole image
         
         psnr_img = peak_signal_noise_ratio(label, recon)
         
         # SSIM on the whole image
         
-        ssim_img = structural_similarity(label, recon)
+        ssim_img = structural_similarity(label, recon, channel_axis=2)
 
         # LPIPS VGG
-        
-        lpips_vgg = loss_fn_vgg(torch.tensor(label).unsqueeze(0).unsqueeze(0).to(device), torch.tensor(recon).unsqueeze(0).unsqueeze(0).to(device)).item()
+
+        lpips_vgg = loss_fn_vgg(label_tensor, recon_tensor).item()
         
         # LPIPS AlexNet
-        lpips_alex = loss_fn_alex(torch.tensor(label).unsqueeze(0).unsqueeze(0).to(device), torch.tensor(recon).unsqueeze(0).unsqueeze(0).to(device)).item()
+
+        lpips_alex = loss_fn_alex(label_tensor, recon_tensor).item()
         
-        # crc_list.append(crc_err)
         psnr_img_list.append(psnr_img)
         ssim_img_list.append(ssim_img)
         lpips_vgg_list.append(lpips_vgg)
         lpips_alex_list.append(lpips_alex)
         
     res_dict['psnr_normal'] = psnr_normal_list
-    res_dict['crc'] = crc_list
     res_dict['psnr_img'] = psnr_img_list
     res_dict['ssim_img'] = ssim_img_list
     res_dict['lpips_vgg'] = lpips_vgg_list
     res_dict['lpips_alex'] = lpips_alex_list
-    
-    res_dict['crc_component'] = crc_component_list
-    res_dict['area_component'] = area_component_list
 
     return res_dict
 
 
-label_root = Path(f'/home/modrzyk/code/blind-dps/results/ffhq/hybrid4/{task}/label/')
-recon_root_ours = Path(f'/home/modrzyk/code/blind-dps/results/ffhq/hybrid4/{task}/recon/')
-recon_root_posterior_sampling = Path(f'/home/modrzyk/code/diffusion-posterior-sampling/results/ffhq/{task}/recon')
-recon_root_richarson_lucy = Path(f'/home/modrzyk/code/blind-dps/results/ffhq/fast-refine/{task}/recon/')
+label_root = Path(f'/home/modrzyk/code/data/EUSIPCO_2024/label/')
+recon_root_ours = Path(f'/home/modrzyk/code/data/EUSIPCO_2024/recon-rl-diffusion-dps-refinement/')
+recon_root_posterior_sampling = Path(f'/home/modrzyk/code/data/EUSIPCO_2024/recon-dps/')
+recon_root_richarson_lucy = Path(f'/home/modrzyk/code/data/EUSIPCO_2024/recon-rl-tv/')
+recon_root_pnp_admm = Path(f'/home/modrzyk/code/data/EUSIPCO_2024/recon-pnp-admm/')
 
 res_ours = stats(label_root, recon_root_ours)
 res_posterior_sampling = stats(label_root, recon_root_posterior_sampling)
 res_richardson_lucy = stats(label_root, recon_root_richarson_lucy)
-
-df_ours = pd.DataFrame({'Area': res_ours['area_component'], 'CRC Error': res_ours['crc_component']})
-df_posterior_sampling = pd.DataFrame({'Area': res_posterior_sampling['area_component'], 'CRC Error': res_posterior_sampling['crc_component']})
-df_richardson_lucy = pd.DataFrame({'Area': res_richardson_lucy['area_component'], 'CRC Error': res_richardson_lucy['crc_component']})
-
-mean_crc_by_area_ours = df_ours.groupby('Area')['CRC Error'].mean()
-mean_crc_by_area_posterior_sampling = df_posterior_sampling.groupby('Area')['CRC Error'].mean()
-mean_crc_by_area_richardson_lucy = df_richardson_lucy.groupby('Area')['CRC Error'].mean()
-
-
-# plt.scatter(res_ours['area_component'], res_ours['crc_component'], label='Ours', alpha=0.5)
-# plt.scatter(res_posterior_sampling['area_component'], res_posterior_sampling['crc_component'], label='DPS', alpha=0.5)
-# plt.scatter(res_richardson_lucy['area_component'], res_richardson_lucy['crc_component'], label='Richardson-Lucy', alpha=0.5)
-
-# plt.xlabel('Area')
-# plt.ylabel('CRC Error')
-# plt.title('CRC Error vs. Area')
-# plt.grid(True)
-# plt.legend()
-# plt.show()
+res_pnp_admm = stats(label_root, recon_root_pnp_admm)
 
 # Calculate average
-# psnr_normal_avg_ours = np.mean(res_ours['psnr_normal'])
-# crc_avg_ours = np.mean(res_ours['crc'])
+
 psnr_img_ours = np.mean(res_ours['psnr_img'])
 ssim_img_ours = np.mean(res_ours['ssim_img'])
 lpips_vgg_ours = np.mean(res_ours['lpips_vgg'])
 lpips_alex_ours = np.mean(res_ours['lpips_alex'])
 
-# psnr_normal_avg_ours_posterior_sampling = np.mean(res_posterior_sampling['psnr_normal'])
-# crc_avg_posterior_sampling = np.mean(res_posterior_sampling['crc'])
 psnr_img_posterior_sampling = np.mean(res_posterior_sampling['psnr_img'])
 ssim_img_posterior_sampling = np.mean(res_posterior_sampling['ssim_img'])
 lpips_vgg_posterior_sampling = np.mean(res_posterior_sampling['lpips_vgg'])
 lpips_alex_posterior_sampling = np.mean(res_posterior_sampling['lpips_alex'])
 
-# psnr_normal_avg_richardson_lucy = np.mean(res_richardson_lucy['psnr_normal'])
-# crc_avg_richardson_lucy = np.mean(res_richardson_lucy['crc'])
 psnr_img_richardson_lucy = np.mean(res_richardson_lucy['psnr_img'])
 ssim_img_richardson_lucy = np.mean(res_richardson_lucy['ssim_img'])
 lpips_vgg_richardson_lucy = np.mean(res_richardson_lucy['lpips_vgg'])
 lpips_alex_richardson_lucy = np.mean(res_richardson_lucy['lpips_alex'])
 
+psnr_img_pnp_admm = np.mean(res_pnp_admm['psnr_img'])
+ssim_img_pnp_admm = np.mean(res_pnp_admm['ssim_img'])
+lpips_vgg_pnp_admm = np.mean(res_pnp_admm['lpips_vgg'])
+lpips_alex_pnp_admm = np.mean(res_pnp_admm['lpips_alex'])
+
 # Calculate standard deviation
-# psnr_normal_std_ours = np.std(res_ours['psnr_normal'])
-# crc_std_ours = np.std(res_ours['crc'])
+
 psnr_img_std_ours = np.std(res_ours['psnr_img'])
 ssim_img_std_ours = np.std(res_ours['ssim_img'])
 lpips_vgg_std_ours = np.std(res_ours['lpips_vgg'])
 lpips_alex_std_ours = np.std(res_ours['lpips_alex'])
 
-# psnr_normal_std_posterior_sampling = np.std(res_posterior_sampling['psnr_normal'])
-# crc_std_posterior_sampling = np.std(res_posterior_sampling['crc'])
 psnr_img_std_posterior_sampling = np.std(res_posterior_sampling['psnr_img'])
 ssim_img_std_posterior_sampling = np.std(res_posterior_sampling['ssim_img'])
 lpips_vgg_std_posterior_sampling = np.std(res_posterior_sampling['lpips_vgg'])
 lpips_alex_std_posterior_sampling = np.std(res_posterior_sampling['lpips_alex'])
 
-# psnr_normal_std_richardson_lucy = np.std(res_richardson_lucy['psnr_normal'])
-# crc_std_richardson_lucy = np.std(res_richardson_lucy['crc'])
 psnr_img_std_richardson_lucy = np.std(res_richardson_lucy['psnr_img'])
 ssim_img_std_richardson_lucy = np.std(res_richardson_lucy['ssim_img'])
 lpips_vgg_std_richardson_lucy = np.std(res_richardson_lucy['lpips_vgg'])
 lpips_alex_std_richardson_lucy = np.std(res_richardson_lucy['lpips_alex'])
 
-# # Define the metrics and their averages
-# metrics = ['PSNR Normal', 'CRC', 'PSNR Image', 'SSIM Image']
-# averages_ours = [psnr_normal_avg_ours, crc_avg_ours, psnr_img_avg_ours, ssim_img_avg_ours]
-# averages_posterior_sampling = [psnr_normal_avg_ours_posterior_sampling, crc_avg_ours_posterior_sampling, psnr_img_avg_ours_posterior_sampling, ssim_img_avg_ours_posterior_sampling]
-# std_devs_ours = [psnr_normal_std_ours, crc_std_ours, psnr_img_std_ours, ssim_img_std_ours]
-# std_devs_posterior_sampling = [psnr_normal_std_posterior_sampling, crc_std_posterior_sampling, psnr_img_std_posterior_sampling, ssim_img_std_posterior_sampling]
+psnr_img_std_pnp_admm = np.std(res_pnp_admm['psnr_img'])
+ssim_img_std_pnp_admm = np.std(res_pnp_admm['ssim_img'])
+lpips_vgg_std_pnp_admm = np.std(res_pnp_admm['lpips_vgg'])
+lpips_alex_std_pnp_admm = np.std(res_pnp_admm['lpips_alex'])
 
-# x = np.arange(len(metrics))
-# bar_width = 0.35
-
-# plt.figure(figsize=(10, 8))
-# # Plot the bar plot
-# plt.bar(x + bar_width/2, averages_ours, bar_width, yerr=std_devs_ours, capsize=2, label='Ours')
-# plt.bar(x - bar_width/2, averages_posterior_sampling, bar_width, yerr=std_devs_posterior_sampling, capsize=2, label='Posterior Sampling')
-
-# plt.xticks(x, metrics)
-# plt.xlabel('Metrics')
-# plt.ylabel('Scores')
-# plt.title('Metrics Comparison')
-# plt.legend()
-
-# plt.show()
-
-# print(f'PSNR Normal Ours: {psnr_normal_avg_ours:.2f} ± {psnr_normal_std_ours:.2f}')
-# print(f'CRC Error Ours: {crc_avg_ours:.2f} ± {crc_std_ours:.2f}')
 print(f'PSNR Image Ours: {psnr_img_ours:.2f} ± {psnr_img_std_ours:.2f}')
 print(f'SSIM Image Ours: {ssim_img_ours:.2f} ± {ssim_img_std_ours:.2f}')
 print(f'LPIPS VGG Ours: {lpips_vgg_ours:.2f} ± {lpips_vgg_std_ours:.2f}')
 print(f'LPIPS AlexNet Ours: {lpips_alex_ours:.2f} ± {lpips_alex_std_ours:.2f}')
 print('---')
-# print(f'PSNR Normal Posterior Sampling: {psnr_normal_avg_ours_posterior_sampling:.2f} ± {psnr_normal_std_posterior_sampling:.2f}')
-# print(f'CRC Error Posterior Sampling: {crc_avg_posterior_sampling:.2f} ± {crc_std_posterior_sampling:.2f}')
 print(f'PSNR Image Posterior Sampling: {psnr_img_posterior_sampling:.2f} ± {psnr_img_std_posterior_sampling:.2f}')
 print(f'SSIM Image Posterior Sampling: {ssim_img_posterior_sampling:.2f} ± {ssim_img_std_posterior_sampling:.2f}')
 print(f'LPIPS VGG Posterior Sampling: {lpips_vgg_posterior_sampling:.2f} ± {lpips_vgg_std_posterior_sampling:.2f}')
 print(f'LPIPS AlexNet Posterior Sampling: {lpips_alex_posterior_sampling:.2f} ± {lpips_alex_std_posterior_sampling:.2f}')
 print('---')
-# print(f'PSNR Normal Richardson Lucy: {psnr_normal_avg_richardson_lucy:.2f} ± {psnr_normal_std_richardson_lucy:.2f}')
-# print(f'CRC Error Richardson Lucy: {crc_avg_richardson_lucy:.2f} ± {crc_std_richardson_lucy:.2f}')
 print(f'PSNR Image Richardson Lucy: {psnr_img_richardson_lucy:.2f} ± {psnr_img_std_richardson_lucy:.2f}')
 print(f'SSIM Image Richardson Lucy: {ssim_img_richardson_lucy:.2f} ± {ssim_img_std_richardson_lucy:.2f}')
 print(f'LPIPS VGG Richardson Lucy: {lpips_vgg_richardson_lucy:.2f} ± {lpips_vgg_std_richardson_lucy:.2f}')
 print(f'LPIPS AlexNet Richardson Lucy: {lpips_alex_richardson_lucy:.2f} ± {lpips_alex_std_richardson_lucy:.2f}')
+print('---')
+print(f'PSNR Image PNP ADMM: {psnr_img_pnp_admm:.2f} ± {psnr_img_std_pnp_admm:.2f}')
+print(f'SSIM Image PNP ADMM: {ssim_img_pnp_admm:.2f} ± {ssim_img_std_pnp_admm:.2f}')
+print(f'LPIPS VGG PNP ADMM: {lpips_vgg_pnp_admm:.2f} ± {lpips_vgg_std_pnp_admm:.2f}')
+print(f'LPIPS AlexNet PNP ADMM: {lpips_alex_pnp_admm:.2f} ± {lpips_alex_std_pnp_admm:.2f}')